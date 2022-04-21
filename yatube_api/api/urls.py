0dfc28ab8dca8b3d70a82c4ff468db12cf04c055
BnamoRS <rbutskih@mail.ru>
2022-04-21 23:54:41 +0300

    дописал модели, написал скелет views, urls, serializer

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import PostView, GroupView, CommentView, FollowViews
+
+
+router = routers.DefaultRouter()
+
+router.register('v1/posts', PostView, basename='post')
+router.register('v1/groups',GroupView, basename='group')
+router.register('v1/comments', CommentView, basename='comment')
+router.register('v1/follows', FollowViews, basename='follow')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,24 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from api.serializers import PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
+from posts.models import Post, Group, Comment, Follow
+
+
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class GroupView(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class FollowViews(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
