e09900724a1db001ce62cb0483bfaea1e6938be4
BnamoRS <rbutskih@mail.ru>
2022-04-29 16:40:03 +0300

    перед 3 ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,19 +9,20 @@ class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        read_only=True,
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
-        read_only = True
+        fields = '__all__'
+        read_only_fields = ('post',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name='posts',
         blank=True,
         null=True
